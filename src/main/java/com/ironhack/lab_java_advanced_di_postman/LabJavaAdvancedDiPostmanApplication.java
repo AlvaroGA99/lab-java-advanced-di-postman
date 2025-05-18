9040bce9a628559f9f5823ff0655e7b3494abc2e
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-05-18 19:42:40 +0200

    Initial commit

---
+++ src/main/java/com/ironhack/lab_java_advanced_di_postman/LabJavaAdvancedDiPostmanApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.lab_java_advanced_di_postman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaAdvancedDiPostmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabJavaAdvancedDiPostmanApplication.class, args);
+	}
+
+}
